c40f347d3b21160cccd5ffa4dee306dd02c3865f
Muswera <patrickobel63@yahoo.com>
2026-03-31 01:43:30 -0400

    Display reviews

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,10 @@
-import ChatBot from './components/chat/ChatBot';
+import ReviewList from './components/reviews/ReviewList';
 
 // connecting frontend to the backend
 function App() {
    return (
       <div className="p-4 h-screen w-full">
-         <ChatBot />
+         <ReviewList productId={1} />
       </div>
    );
 }
