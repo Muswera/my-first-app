9cf224136831a103fd37845156df0ded66b73020
Muswera <patrickobel63@yahoo.com>
2026-05-19 10:31:52 -0400

    Re-installed bun

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -33,5 +33,5 @@ const port = process.env.PORT || 3000;
 // });
 
 app.listen(port, () => {
-   console.log(`Server is running on port ${port}`);
+   console.log(`Server is running on port http://localhost:${port}`);
 });
